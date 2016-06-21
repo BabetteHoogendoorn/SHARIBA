eba392ffa3781e0b4bf68e10512bdffeb835f3ed
Babette Hoogendoorn <babettehoogendoorn@hotmail.com>
2016-06-21 15:44:13 +0200

    added city.js

--- routes/city.js
+++ routes/city.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET city page. */
+router.get('/', function(req, res) {
+  res.render('city', { title: 'Express' });
+});
+
 
 module.exports = router;
