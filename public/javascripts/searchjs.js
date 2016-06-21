379caf3cc284c669e3c19d40dd91c277b62c5483
Sharon <sharonvandergeest@live.nl>
2016-06-21 15:51:31 +0200

    added two files

---
+++ public/javascripts/searchjs.js
@@ -0,0 +1,30 @@
+var fireRequest = true;
+
+$ ( document ).ready( function () {
+	console.log('dom is ready')
+
+	$ ( '#searchform' ).on ( "keyup",function (){
+		var inputLetters = {
+			searchTyping: $ ('#searchform') .val( )
+		}
+		$('#foundPlaces').empty()
+			if(fireRequest) {
+				fireRequest = false
+				$.post ('search/ajaxSearch', inputLetters, function(data){
+
+					for (person in data){
+						console.log(data[person].firstname)
+						$ ( '#foundPlaces' ).append( '<div class="newCity">' + data[person].firstname + " " + 
+							data[person].lastname + '</div>' )
+					}
+					$ ( '.newCity' ) .click( function() { 
+						$('#searchform').val($(this).text()) 
+					})
+				})
+				setTimeout(function(){
+					fireRequest = true
+				}, 300)
+			}
+
+})
+})
