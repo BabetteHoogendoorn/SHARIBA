cc5d795ae0d662eb0f8fb64433bd1188e6a28f58
Babette Hoogendoorn <babettehoogendoorn@hotmail.com>
2016-06-21 17:58:56 +0200

    added more cities but have to fix some problems

--- routes/city.js
+++ routes/city.js
@@ -7,4 +7,39 @@ router.get('/', function(req, res) {
 });
 
 
+
 module.exports = router;
+
+
+
+
+
+  Promise.all([
+    country.create({
+      name: 'Netherlands'
+    }).then(function(thecountry){
+      city.create({
+        name:'Amsterdam',
+        countryId: thecountry.id}
+        ),
+      city.create({
+        name:'Eindhoven',
+        countryId: thecountry.id}
+        ).then(function(thecity){
+          cityTip.create({
+            title:'Top spot',
+            body:'This place is awesome!',
+            user_id: 1
+          })
+        })
+      }),
+    country.create({
+      name:'Austria'
+    }).then(function(thecountry){
+     city.create({
+      name:'Salzburg',
+      countryId: thecountry.id
+    })
+   })
+    ])
+})
