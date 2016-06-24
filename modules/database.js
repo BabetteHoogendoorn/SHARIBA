1b29b2ba94c10d7bce11deaa0cf3ae13491f0926
Sarith Breedijk <sarith@live.nl>
2016-06-24 16:26:17 +0200

    fixed db

--- modules/database.js
+++ modules/database.js
@@ -1,14 +1,17 @@
 var pg = require('pg');
+
+
 // container object
+
 var db = {
-	mod: {}
+  mod: {}
 }
 
 //set up sql
 var pg = require('pg');
 var Sequelize = require('sequelize');
-db.conn = new Sequelize('shariba', process.env.POSTGRES_USER,
-	process.env.POSTGRES_PASSWORD, {
+db.conn = new Sequelize('shariba', process.env.POSTGRES_USER, 
+  process.env.POSTGRES_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
   define: {
@@ -98,18 +101,18 @@ db.cityTip = db.conn.define('cityTip', {
 
 
 //establish relationships
-
 db.country.hasMany(db.city)
 db.city.belongsTo(db.country)
 db.user.hasMany(db.cityTip)
 db.cityTip.belongsTo(db.user)
 db.city.hasMany(db.cityTip)
 
-db.conn.sync({force: true
+//synchronize with database
+db.conn.sync({
+ force: true
 }).then(function() {
-	console.log('sync done')
-}).then(function(){
-	Promise.all([
+ console.log('sync done')
+ Promise.all([
   db.user.create({
     name: 'kip',
     email: 'kip',
@@ -120,8 +123,8 @@ db.conn.sync({force: true
     }).then(function(thecountry){
       db.city.create({
         name:'Amsterdam',
-        countryId: thecountry.id
-      }),
+        countryId: thecountry.id}
+        ),
       db.city.create({
         name:'Eindhoven',
         countryId: thecountry.id}
@@ -139,279 +142,9 @@ db.conn.sync({force: true
      db.city.create({
       name:'Salzburg',
       countryId: thecountry.id
-        })
-   }),
-		db.country.create({
-			name: 'Austria'
-		}).then(function(thecountry){
-			city.create({
-				name:'Vienna',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Belgium'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Brussels',
-				countryId: thecountry.id
-			}).then(function(thecity){
-				db.cityTip.create({
-					title:'This little guy',
-					body:'He pees in a fountain',
-					user_id: 2
-				})
-			})
-    }),
-
-		db.country.create({
-			name:'Bulgaria'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Sofia',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Croatia'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Zagreb',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Cyprus Republic'
-		}).then(function(thecountry){
-			db.city.create({
-			name:'Cyprus Republic'
-		}).then(function(thecountry){
-				name:'Nicosia',
-        countryId: thecountry.id
-			
-			})
-		}),
-		db.country.create({
-			name:'Czech Republic'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Prague',
-				countryId: thecountry.id
-			}).then(function(thecity){
-				db.cityTip.create({
-					title:'drinks at this bar',
-					body:'Come here for the cheapest beers',
-					user_id: 3
-				})
-			})
-		}),
-		db.country.create({
-			name:'Denmark'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Copenhagen',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Estonia'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Tallinn',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Finland'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Helsinki',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'France'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Paris',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Germany'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Berlin',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Greece'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Athens',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Hungary'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Budapest',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Ireland'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Dublin',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Italy'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Rome',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Latvia'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Riga',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Lithuania'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Vilnius',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Luxembourg'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Luxembourg',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Malta'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Valletta',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Netherlands'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Amsterdam',
-				countryId: thecountry.id
-			}).then(function(anothercity) {
-				db.city.create({
-					name:'Eindhoven',
-					countryId: thecountry.id
-				}).then(function(thecity){
-					db.cityTip.create({
-						title:'Top spot',
-						body:'This place is awesome!',
-						user_id: 1
-					})
-				})
-			})
-		}),
-		db.country.create({
-			name:'Poland'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Warsaw',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Portugal'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Lisbon',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Romania'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Bucharest',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Slovakia'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Bratislava',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Slovena'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Ljubljana',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Spain'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Madrid',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Sweden'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Stockholm',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'United Kingdom'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'London',
-				countryId: thecountry.id
-			})
-		}),
-		db.country.create({
-			name:'Vatican City'
-		}).then(function(thecountry){
-			db.city.create({
-				name:'Vatican City',
-				countryId: thecountry.id
-			}).then(function(thecity){
-				db.cityTip.create({
-					title:'This place is holy',
-					body:'I have seen the pope, such a nice guy!',
-					user_countryId: 4
-				})
-			})
-		});
-	
-
-
- console.log('sync done')
- 
-
-
+    })
+   })
+    ])
+});
 
 module.exports = db
--- routes/city.js
+++ routes/city.js
@@ -5,27 +5,27 @@ var db = require('../modules/database');
 var pg = require('pg');
 var Sequelize = require('sequelize');
 
-// Set the view engine
-router.set( 'views', 'views' )
-router.set( 'view engine', 'pug' )
+// // Set the view engine
+// router.set( 'views', 'views' )
+// router.set( 'view engine', 'pug' )
 
 /* GET city page. */
 router.get('/city', function(req, res) {
-  Promise.all([
-    country.findAll({
-      include: [
-        {model: db.country},
-        {model: deb.city,
-        include: {model: db.cityTip,
-          include: {model: db.user}
-        }]
-    })then.(function(list) {
-      response.render('city', {
-        country: list
-      })
-    })
-  })
-})
-
+  // Promise.all([
+  //   country.findAll({
+  //     include: [
+  //       {model: db.country},
+  //       {model: deb.city,
+  //       include: {model: db.cityTip,
+  //         include: {model: db.user}
+  //       }]
+  //   })then.(function(list) {
+  //     response.render('city', {
+  //       country: list
+  //     })
+  //   })
+  // }
+});
+;
 
 module.exports = router;
